dba59a322d1d5a4c666b133ef19f0fa692f730a7
Hossam Khair <hossam.khir1@outlook.com>
2022-09-22 05:07:53 +0200

    feat: practicing url patterns

--- source/pages/views.py
+++ source/pages/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(req, *args, **kwargs):
+    print(req.user)
     return HttpResponse("<h1>Hello, World!</h1>")
+
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
