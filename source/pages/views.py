fb0ec494a922f5c48fed32ac20888a2abe4b679e
Hossam Khair <hossam.khir1@outlook.com>
2022-09-22 05:15:50 +0200

    feat: adding templates to the app

--- source/pages/views.py
+++ source/pages/views.py
@@ -5,12 +5,17 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(req, *args, **kwargs):
     print(req.user)
-    return HttpResponse("<h1>Hello, World!</h1>")
+    # return HttpResponse("<h1>Hello, World!</h1>")
+    return render(req, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(req, *args, **kwargs):
+    # return HttpResponse("<h1>Contact Page</h1>")
+    return render(req, "contact.html", {})
 
 
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
+
+def about_view(req, *args, **kwargs):
+    # return HttpResponse("<h1>About Page</h1>")
+    return render(req, "about.html", {})
+
