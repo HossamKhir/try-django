567172295c327d744830e48d837e737854b2855f
Hossam Khair <hossam.khir1@outlook.com>
2022-09-24 18:43:50 +0200

    feat: learned about handling DoesNotExist exception

--- source/products/views.py
+++ source/products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # from .forms import RawProductForm
 from .forms import ProductForm
@@ -73,7 +74,11 @@ def render_initial_data(request):
 
 
 def dynamic_lookup_view(request, id):
-    obj = Product.objects.get(id=id)
+    obj = get_object_or_404(Product, id=id)
+    # try:
+    #     obj = Product.objects.get(id=id)
+    # except Product.DoesNotExist:
+    #     raise Http404
     context = {
         "object": obj,
     }
