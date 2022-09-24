cc7445417ccd8c8b93b813cc26a7d3f5e67f985e
Hossam Khair <hossam.khir1@outlook.com>
2022-09-24 17:50:39 +0200

    feat: learned about form widgets

--- source/products/forms.py
+++ source/products/forms.py
@@ -14,6 +14,22 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "some placeholder text",
+            }
+        )
+    )  # by default, required=True
+    description = forms.CharField(
+        label="",
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "new-class-name two",
+                "rows": 20,
+                "cols": 128,
+            }
+        ),
+    )  # make the label blank
+    price = forms.DecimalField(initial=199.99)  # initial value
