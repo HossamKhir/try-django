502f6703e640e0c0f595fb961e47d6ad1f8aa71b
Hossam Khair <hossam.khir1@outlook.com>
2022-09-26 14:11:31 +0200

    feat: how to do a raw detail class-based view

--- source/courses/views.py
+++ source/courses/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import Course
 
 
 # Create your views here.
@@ -8,8 +10,12 @@ def my_fbv(request, *args, **kwargs):
 
 
 class CourseView(View):
-    template_name = "about.html"
+    template_name = "courses/course_detail.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, id=None, *args, **kwargs):
         # return render(request, "about.html", {})
-        return render(request, self.template_name, {})
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context["object"] = obj
+        return render(request, self.template_name, context)
