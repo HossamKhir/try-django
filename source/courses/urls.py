a7dd8d9f4936adefe716de14eb63fe630329e1c7
Hossam Khair <hossam.khir1@outlook.com>
2022-09-26 14:57:53 +0200

    feat: how to do raw list class-based view

--- source/courses/urls.py
+++ source/courses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import my_fbv, CourseView
+from .views import my_fbv, CourseView, CourseListView
 
 app_name = "course"
 
 urlpatterns = [
     # path("", my_fbv, name="course-list"),
     # path("", CourseView.as_view(), name="course-list"),
-    path("", CourseView.as_view(template_name="contact.html"), name="course-list"),
+    # path("", CourseView.as_view(template_name="contact.html"), name="course-list"),
+    path("", CourseListView.as_view(), name="course-list"),
+    path("<int:id>/", CourseView.as_view(), name="course-detail"),
 ]
